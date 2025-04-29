8c58209a7342d513cce9d83039a2d04cb19c9e2d
xalviolab <xalvionlab@gmail.com>
2025-04-29 20:06:51 +0300

    Delete scripts directory

--- scripts/update-feedback-table-status.sql
+++
@@ -1,2 +0,0 @@
--- Feedback tablosuna status sütunu ekle
-ALTER TABLE public.feedback ADD COLUMN IF NOT EXISTS status VARCHAR(50) DEFAULT 'new';
--- scripts/update-feedback-table.sql
+++
@@ -1,2 +0,0 @@
--- Feedback tablosuna feedback_type sütunu ekle
-ALTER TABLE public.feedback ADD COLUMN IF NOT EXISTS feedback_type VARCHAR(50);
